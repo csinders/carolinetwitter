2e1d476561c0b70e8da3ff81f268953418fff901
csinders <csinders@gmail.com>
2013-11-16 14:45:29 -0500

    WE'VE STARTED ZE PARSING

--- app.py
+++ app.py
@@ -2,22 +2,53 @@ import os
 import time
 from twitter import * 
 from flask import Flask, request, render_template, redirect, abort, flash, jsonify
+import json
 
 app = Flask(__name__)  #create our flask app 
 
+CONSUMER_KEY = "ZOWPlakwCO2Xa8rgEvornw"
+CONSUMER_SECRET = "aNoDIKBmNkLqeXh5lJYcazhNRMgj7Mc1EJBZyOcCQA8"
+OAUTH_TOKEN = "22389244-C4xVthVea0N3uMnJ5JdomjJcPJQwZ7ILQIBxJufhR"
+OAUTH_SECRET = "GGvKIjvEaQJeOrgdSEvswVSt21d0ttcOmxuPTucqU3SgM"
+
 #configure Twitter API
-twitter = Twitter(auth=OAuth(os.environ.get('OAUTH_TOKEN'), os.environ.get('OAUTH_SECRET'),
-						os.environ.get('CONSUMER_KEY'), os.environ.get('CONSUMER_SECRET'))
-
-	)
-
-@app.route('/')
-def main():
-
-	return render_template('index.html')
+twitter = Twitter(auth= OAuth (OAUTH_TOKEN, OAUTH_SECRET,
+					CONSUMER_KEY, CONSUMER_SECRET))
 
 
+@app.route('/<username>')
+def main(username=''):
 
+	usertweets = get_user_tweets(username)
+	sort_tweets_by_day(usertweets)
+	templateData = {
+		'tweets' : json.dumps(usertweets, indent=2)
+	}
+	return render_template('index.html', **templateData)
+
+
+
+
+def get_user_tweets(username):
+	return twitter.statuses.user_timeline(screen_name=username)
+
+
+def sort_tweets_by_day(tweets):
+	tweetsPerDate = {}
+
+	for tweet in tweets: 
+		tweet_day = tweet["created_at"].split(' ')[0]
+
+		#create the key for the day if it don't exist 
+		if tweet_day not in tweetsPerDate:
+				tweetsPerDate[tweet_day] = []
+
+		#add that tweet to the list of tweets for that day 
+		tweetsPerDate[tweet_day].append(tweet)
+
+	print([ (x,len(tweetsPerDate[x])) for  x in tweetsPerDate ])
+
+	
 
 #let's start the server, yo!
 if __name__ == "__main__":
